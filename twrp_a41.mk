938521671b290c2e3c94a65b0c88e6b5bfc2fc25
Zillion <DevZillion@proton.me>
2024-02-18 20:38:35 +0200

    a41: drop device.mk usage

--- twrp_a41.mk
+++ twrp_a41.mk
@@ -23,8 +23,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/a41/device.mk)
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
 # Charger
 PRODUCT_PACKAGES += \
